bc0e085a1af15cdc653182f495d66ca35659f37d
Jonathan Soo <jcsoo@agora.com>
2020-09-19 14:50:24 -0400

    Cleanup uart example

--- examples/uart.rs
+++ examples/uart.rs
@@ -6,8 +6,7 @@
 extern crate cortex_m;
 extern crate cortex_m_rt as rt;
 extern crate stm32g4xx_hal as hal;
-use panic_rtt_target as _;
-
+extern crate panic_halt;
 
 use core::fmt::Write;
 
@@ -15,23 +14,19 @@ use hal::prelude::*;
 use hal::serial::FullConfig;
 use hal::stm32;
 use nb::block;
-use rtt_target::{rprintln, rtt_init_print};
 use rt::entry;
 
 #[entry]
 fn main() -> ! {
-    rtt_init_print!();    
     let dp = stm32::Peripherals::take().expect("cannot take peripherals");
     let mut rcc = dp.RCC.constrain();
     let gpioa = dp.GPIOA.split(&mut rcc);
     let cfg = FullConfig::default();
-    rprintln!("cfg: {:?}", cfg);
     let mut usart = dp
         .USART2
         .usart(gpioa.pa2, gpioa.pa3, cfg, &mut rcc)
         .unwrap();
 
-    rprintln!("Starting uart");
     writeln!(usart, "Hello\r").unwrap();
 
     let mut cnt = 0;
